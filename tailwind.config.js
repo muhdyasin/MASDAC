34f35d6df4257ff7eea9175acd12c387c0735778
Muhammad Yasin Shereef <muhdyasin805@gmail.com>
2026-02-12 12:15:46 +0530

    Initial commit: Tailwind website layout

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: ["./index.html"], 
+  theme: {
+    extend: {
+      colors:{
+        brand:{
+          surfaceDim: "#D8DAE0",
+          surfaceBright: "#F8F9FF",
+          surface: "#F8F9FF",
+          surfaceContainerLowest: "#FFFFFF",
+          surfaceContainerLow: "#F2F3FA",
+          surfaceContainer: "#ECEDF4",
+          surfaceContainerHigh: "#E7E8EE",
+          surfaceContainerHighest: "#E1E2E8"
+        }
+      }
+    },              
+  },
+  plugins: [],               
+}
